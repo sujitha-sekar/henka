b5c3db1c97d3e9c786141ccee4274ac06bb5ded0
sujitha-sekar <snowashii2002@gmail.com>
2024-09-23 14:36:25 +0530

    changes

--- src/app/physics/components/physics-list/physics-list.component.ts
+++ src/app/physics/components/physics-list/physics-list.component.ts
@@ -11,7 +11,7 @@ export class PhysicsListComponent {
   constructor(private router: Router) {}
   earthlist = [
     { image: '../../../../assets/relativity-title.webp', title: 'Work of Realativity', urlLink: 'relativity'},
-    { image: '../../../../assets/winds.jpg', title: 'How to Rockets works?', urlLink: 'rocket-propulsion'},
+    { image: '../../../../assets/rocket-propul.png', title: 'How to Rockets works?', urlLink: 'rocket-propulsion'},
     { image: '../../../../assets/winds.jpg', title: 'Methane & Information about it', urlLink: ''}
   ]
 
